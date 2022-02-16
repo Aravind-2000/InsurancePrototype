8d65f4ad6215d1003130c12682a7917f1de22671
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-02-16 09:58:39 +0530

    Letter table

---
+++ src/main/java/com/example/insuranceprototype/Entity/Lettertable.java
@@ -0,0 +1,33 @@
+package com.example.insuranceprototype.Entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Lettertable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String letterType;
+
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+    private String field5;
+    private String field6;
+    private String field7;
+    private String field8;
+    private String field9;
+
+}
