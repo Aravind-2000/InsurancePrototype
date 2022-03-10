dd0899ad01390226036ec42bc7b44162718bd21a
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-10 17:42:23 +0530

    added a new method which will convert the file into byte array and save locally

--- src/main/java/com/example/insuranceprototype/pdf/PdfService.java
+++ src/main/java/com/example/insuranceprototype/pdf/PdfService.java
@@ -10,7 +10,13 @@ import com.itextpdf.layout.element.Paragraph;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 
 
@@ -79,4 +85,13 @@ public class PdfService {
         document.close();
         return filepath;
     }
+
+    public Path uploadToDirectory(MultipartFile file) throws IOException{
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get(("src/main/resources/resumefiles/") + file.getOriginalFilename());
+        Files.write(path,bytes);
+        return path;
+    }
+
+
 }
