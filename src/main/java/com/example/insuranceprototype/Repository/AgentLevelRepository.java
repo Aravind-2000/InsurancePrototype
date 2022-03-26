382a44ab8ebeb94cb2278202773243c89a07c199
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-26 12:17:16 +0530

    Agent Type level table integration.

---
+++ src/main/java/com/example/insuranceprototype/Repository/AgentLevelRepository.java
@@ -0,0 +1,7 @@
+package com.example.insuranceprototype.Repository;
+
+import com.example.insuranceprototype.Entity.AgentTypeLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentLevelRepository extends JpaRepository<AgentTypeLevel, Long> {
+}
