792d1728183203bfd6c2d7cc4629c22ddd641d2e
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-30 15:35:10 +0530

    Small modifications

--- src/main/java/com/example/insuranceprototype/Controller/CompanyController.java
+++ src/main/java/com/example/insuranceprototype/Controller/CompanyController.java
@@ -4,17 +4,11 @@ import java.util.List;
 import com.example.insuranceprototype.Entity.Company;
 import com.example.insuranceprototype.Service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/company")
 public class CompanyController {
 	@Autowired
