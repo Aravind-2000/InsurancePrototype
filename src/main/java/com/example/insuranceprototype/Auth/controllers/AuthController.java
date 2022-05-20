a62f2ae1914e8d163619e5ba84d000e5ba7c85e9
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-05-20 16:19:23 +0530

    Over rided the auth for /ui/ url.

--- src/main/java/com/example/insuranceprototype/Auth/controllers/AuthController.java
+++ src/main/java/com/example/insuranceprototype/Auth/controllers/AuthController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import com.example.insuranceprototype.Auth.exception.TokenRefreshException;
 import com.example.insuranceprototype.Auth.models.RefreshToken;
 import com.example.insuranceprototype.Auth.models.User;
+import com.example.insuranceprototype.Auth.models.UserDTO;
 import com.example.insuranceprototype.Auth.payload.request.LogOutRequest;
 import com.example.insuranceprototype.Auth.payload.request.LoginRequest;
 import com.example.insuranceprototype.Auth.payload.request.SignupRequest;
@@ -16,10 +17,12 @@ import com.example.insuranceprototype.Auth.repository.UserRepository;
 import com.example.insuranceprototype.Auth.security.jwt.JwtUtils;
 import com.example.insuranceprototype.Auth.security.services.RefreshTokenService;
 import com.example.insuranceprototype.Auth.security.services.UserDetailsImpl;
+import com.example.insuranceprototype.Auth.security.services.UserService;
 import com.example.insuranceprototype.Entity.Role;
 import com.example.insuranceprototype.Repository.EmployeeRepository;
 import com.example.insuranceprototype.Repository.PermissionRepository;
 import com.example.insuranceprototype.Repository.RoleRepository;
+import com.example.insuranceprototype.error.ErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -27,11 +30,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -45,7 +44,7 @@ public class AuthController {
   UserRepository userRepository;
 
   @Autowired
-  EmployeeRepository employeeRepository;
+  private UserService userService;
 
   @Autowired
   PasswordEncoder encoder;
@@ -55,6 +54,9 @@ public class AuthController {
 
   @Autowired
   private RoleRepository roleRepo;
+
+  @Autowired
+  private ErrorService errorService;
 
   @Autowired
   RefreshTokenService refreshTokenService;
@@ -130,6 +132,39 @@ public class AuthController {
 
   }
 
+  @GetMapping("/user/{id}")
+  public ResponseEntity<?> getUser(@PathVariable Long id){
+     User user = userRepository.findById(id).get();
+     UserDTO dto = userService.listUser(user);
+     return ResponseEntity.ok(dto);
+  }
+
+  @PatchMapping("/user/{id}")
+  public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User newDetails){
+
+    User user = userRepository.getById(id);
+
+    if(newDetails.getUsername() != null){
+      user.setUsername(newDetails.getUsername());
+    }
+
+    if(newDetails.getEmail() != null){
+      user.setEmail(newDetails.getEmail());
+    }
+
+    if(newDetails.getRoleId() != null){
+      user.setRoleId(newDetails.getRoleId());
+    }
+
+    if(newDetails.getAgentId() != null){
+      user.setAgentId(newDetails.getAgentId());
+    }
+
+    userRepository.save(user);
+    return ResponseEntity.ok(errorService.getErrorById("ER003"));
+  }
+
+
 //  @PostMapping("TokenCheck")
 //  public ResponseEntity<?> TokenCheck(@RequestBody String jwt){
 //
@@ -137,5 +172,7 @@ public class AuthController {
 //    return ResponseEntity.ok(new MessageResponse("Invalid access token"));
 //
 //  }
+
+
 
 }
